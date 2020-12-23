85338dd84ae1c72e718be96828c86fb1eb9291fe
Vengelis <gaby17250@gmail.com>
2020-12-23 16:15:25 +0100

    Adding Page Module

---
+++ nexus/pages/admin/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+include("system/security/PA_checkup.php");
+include("system/designer/PA_menu_top.php");
+?>
+<div class="bg-gray-700 overflow-hidden w-full">
+    <div class="px-4 py-5 sm:p-6">
+        <h2 class="text-xl text-white">Pages</h2>
+        <h3 class="text-sm text-gray-200">Gestion des pages d'accueil</h3>
+    </div>
+</div>
+<?php
+include("system/designer/PA_menu_bottom.php");
+?>
