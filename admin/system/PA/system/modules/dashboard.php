332916e601b0d00c4100d496d7cc40e9ac42d804
Vengelis <gaby17250@gmail.com>
2020-12-23 17:22:58 +0100

    Adding plugins dashboard

--- admin/system/PA/system/modules/dashboard.php
+++ admin/system/PA/system/modules/dashboard.php
@@ -5,7 +5,7 @@ include("system/designer/PA_menu_top.php");
 <div class="bg-gray-700 overflow-hidden w-full">
     <div class="px-4 py-5 sm:p-6">
         <h2 class="text-xl text-white">Modules</h2>
-        <h3 class="text-sm text-gray-200">Centre de gestion des modules. Les modules sont des application qui vont être ajouté au CMS</h3>
+        <h3 class="text-sm text-gray-200">Centre de gestion des modules. Les modules sont des applications qui vont être ajouté au CMS</h3>
     </div>
 </div>
 
