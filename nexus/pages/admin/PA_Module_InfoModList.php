30b3c44881bfc4914501c7e1fefb32a4f4ea6949
Vengelis <gaby17250@gmail.com>
2020-12-23 17:39:08 +0100

    Strong module name

--- nexus/pages/admin/PA_Module_InfoModList.php
+++ nexus/pages/admin/PA_Module_InfoModList.php
@@ -3,7 +3,7 @@
         <div class="px-4 py-4 sm:px-6">
             <div class="flex items-center justify-between">
             <p class="text-sm font-medium text-orange-600 truncate">
-                Pages
+                <strong>Pages</strong>
             </p>
             <div class="ml-2 flex-shrink-0 flex">
                 <p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full" style="background-color: #FCD34D; color: #92400E;">
