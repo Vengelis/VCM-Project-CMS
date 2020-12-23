bc13552e569035f3f6225fb25cce65f257bc87b1
Vengelis <gaby17250@gmail.com>
2020-12-23 14:07:25 +0100

    Delete old project and test area

--- test/testPostMethode/incrust.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(isset($_POST['test']))
-{
-    echo $_POST['test'];
-}
--- test/testPostMethode/test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("incrust.php");
-?>
-<form action="test1.php" method="post" enctype="multipart/form-data">
-<input name="test" id="test"></input>
-<button type="submit">send</button>
-</form>
