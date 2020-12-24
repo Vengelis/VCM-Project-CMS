e7e36ee06d8684e26414de5014b55e5ec72e985b
Vengelis <gaby17250@gmail.com>
2020-12-24 04:50:36 +0100

    JS adding check profil image

--- admin/system/PA/community/members/create.php
+++ admin/system/PA/community/members/create.php
@@ -50,6 +50,7 @@ if(isset($_POST['sended']))
         }
     }
     $userImageProfil = "";
+    var_dump($_FILES);
     if(isset($_FILES))
     {
         
@@ -193,6 +194,26 @@ else
                     </div>
                 </div>
             </div>
+            <div name="alertWrongExt" id="alertWrongExt" class="hidden rounded-md bg-red-50 p-4 lg:col-span-12 m-5">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                    <!-- Heroicon name: x-circle -->
+                    <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                    </svg>
+                    </div>
+                    <div class="ml-3">
+                    <h3 class="text-sm font-medium text-red-800">
+                        Erreur sur l'image !
+                    </h3>
+                    <div class="mt-2 text-sm text-red-700">
+                        <ul class="list-disc pl-5 space-y-1">
+                            <li>Le format de l'image est invalide ! (trop lourde ou mauvaise extension)</li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </div>
             <div name="alertBox" id="alertBox" class="hidden rounded-md bg-red-50 p-4 lg:col-span-12 m-5">
                 <div class="flex">
                     <div class="flex-shrink-0">
@@ -259,7 +280,7 @@ else
                                     <span>Changer</span>
                                     <span class="sr-only"> Photo d'utilisateur</span>
                                     </label>
-                                    <input type="file" id="user_photo" name="user_photo" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md">
+                                    <input type="file" id="user_photo2" name="user_photo" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md">
                                 </div>
                                 </div>
                             </div>
--- system/security/profil_check_upload.php
+++ system/security/profil_check_upload.php
@@ -11,42 +11,46 @@ function get_extension(filename) {
     return filename.split('.').pop().toLowerCase();
 }
 
-document.forms[0].addEventListener('submit', function( evt ) {
-    var file1 = document.getElementById('user_photo1').files[0];
-    if(file1 && file1.size < <?php echo parse_size(ini_get('post_max_size')) ;?>)
-        if(get_extension(file1.val()) == 'jpg' || get_extension(file1.val()) == 'png' || get_extension(file1.val()) == 'gif'){
-        } else {
+$('form').submit(function( e ) {    
+    var size = <?php echo parse_size(ini_get('post_max_size')) ;?>;
+    if(!($('#user_photo')[0].files[0].size < size && (get_extension($('#user_photo').val()) == 'jpg' || get_extension($('#user_photo').val()) == 'png' || get_extension($('#user_photo').val()) == 'gif'))) { // 10 MB (this size is in bytes)
+        document.getElementById("alertWrongExt").className = "rounded-md bg-red-50 p-4 lg:col-span-12 m-5";
+        e.preventDefault();
+    }
+    if(!($('#user_photo2')[0].files[0].size < size && (get_extension($('#user_photo2').val()) == 'jpg' || get_extension($('#user_photo2').val()) == 'png' || get_extension($('#user_photo2').val()) == 'gif'))) { // 10 MB (this size is in bytes)
+        document.getElementById("alertWrongExt").className = "rounded-md bg-red-50 p-4 lg:col-span-12 m-5";
+        e.preventDefault();
+    }
+});
+
+/*function checkFile(env) {
+    //var size = <?php echo parse_size(ini_get('post_max_size')) ;?>;
+    var size = 2000;
+    var filelits = env.target.files;
+    var file = filelits[0];
+    alert(env);
+    if(file.size < size)
+    {   
+        if(get_extension(file.val()) == 'jpg' || get_extension(file.val()) == 'png' || get_extension(file.val()) == 'gif')
+        {
+            // ok
+        } 
+        else 
+        {
             <?php
             $alertMessage .= "<li>L'extension d'image n'est pas correcte.</li>";
             ?>
             document.getElementById("alertBox").className = "rounded-md bg-red-50 p-4 lg:col-span-12 m-5";
-            evt.preventDefault();
+            file.val() = "";
         }
-    } else {
+    } 
+    else 
+    {
         <?php
         $alertMessage .= "<li>La taille de l'image est trop grande.</li>";
         ?>
         document.getElementById("alertBox").className = "rounded-md bg-red-50 p-4 lg:col-span-12 m-5";
-        evt.preventDefault();
+        file.val() = "";
     }
-
-    var file2 = document.getElementById('user_photo2').files[0];
-
-    if(file2 && file2.size < <?php echo parse_size(ini_get('post_max_size')) ;?>)
-        if(get_extension(file2.val()) == 'jpg' || get_extension(file2.val()) == 'png' || get_extension(file2.val()) == 'gif'){
-        } else {
-            <?php
-            $alertMessage .= "<li>L'extension d'image n'est pas correcte.</li>";
-            ?>
-            document.getElementById("alertBox").className = "rounded-md bg-red-50 p-4 lg:col-span-12 m-5";
-            evt.preventDefault();
-        }        
-    } else {
-        <?php
-        $alertMessage .= "<li>La taille de l'image est trop grande.</li>";
-        ?>
-        document.getElementById("alertBox").className = "rounded-md bg-red-50 p-4 lg:col-span-12 m-5";
-        evt.preventDefault();
-    }
-}, false);
+}*/
 </script>
