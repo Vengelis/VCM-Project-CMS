4f3c5f55201f9ccd4896c6dd26dba6a9aacbe9b6
Vengelis <gaby17250@gmail.com>
2021-01-01 20:15:24 +0100

    Adding multiples fonts

--- system/designer/head.php
+++ system/designer/head.php
@@ -29,6 +29,8 @@ if(!isset($exe))
         <script src="https://cdn.jsdelivr.net/combine/npm/highlightjs@9.16.2/highlight.pack.min.js,npm/axios@0.19.2/dist/axios.min.js,npm/dlv@1.1.3/dist/dlv.umd.min.js,gh/alpinejs/alpine@v2.2.1/dist/alpine.js,npm/fuse.js@5.0.9-beta/dist/fuse.min.js"></script>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
+        <link rel="stylesheet" href="system/designer/styles/fonts.css"/>
+
         <!-- Meta -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -43,4 +45,4 @@ if(!isset($exe))
         <meta property="og:description" content="VCM-Project - Embed en cours de construction. i lsera modifiable via le panel d'administration" />
         <meta name="theme-color" content="#ff5a1f">
     </head>
-    <body class="h-screen">
+    <body class="h-screen font-nunito">
