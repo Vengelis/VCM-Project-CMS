e93d71de5c67fc3550abd4ca3f56eb84aa1b0f1c
Theo Samson <ace.theosamson@gmail.com>
2021-01-16 01:06:40 +0100

    patch menu; topic v1

--- nexus/forum/index/accueil.php
+++ nexus/forum/index/accueil.php
@@ -29,7 +29,7 @@ if(!isset($exe))
         <svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
         </svg>
-        <a href="http://cms/VCM-Project-CMS/index.php?app=nexus&mod=forum&ctl=index&cmpt=acceuil" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Forum</a>
+        <a href="http://cms/VCM-Project-CMS/index.php?app=nexus&mod=forum&ctl=index&cmpt=accueil" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Forum</a>
       </div>
     </li>
   </ol>
